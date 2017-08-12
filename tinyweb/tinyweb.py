e65d3a48056a970b8fe6c368eac06d9e70ef8883
Do Hoerin <hellodhlyn@gmail.com>
2017-08-13 01:39:49 +0900

    Fixed endpoint collision error (and more)   - Added feature: pass url query params   - Handle JSONDecodeError as 500 Internal Server Error

--- tinyweb/tinyweb.py
+++ tinyweb/tinyweb.py
@@ -1,8 +1,12 @@
+from json import JSONDecodeError
 from typing import List
+from urllib.parse import urlencode
 
 import certifi
 import urllib3
-from flask import Flask, render_template, json
+from flask import Flask, render_template, json, request, abort
+
+http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 
 
 class Route:
@@ -23,14 +27,20 @@ class Route:
                      response from ctx_path will be pass to template with
                      name ``context``
     """
+
     def __init__(self, path: str, template: str, ctx_path: str = None):
         def __f():
             if ctx_path is None:
                 return render_template(template)
             else:
-                http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',
-                                           ca_certs=certifi.where())
-                ctx = json.loads(http.request('GET', ctx_path).data)
+                url = ctx_path
+                if request.args:
+                    url += ('?' + urlencode(request.args))
+
+                try:
+                    ctx = json.loads(http.request('GET', url).data)
+                except JSONDecodeError:
+                    return abort(500)
 
                 return render_template(template, context=ctx)
 
@@ -46,8 +56,9 @@ def start(__name__, routes: List[Route], host: str):
         if __name__ == '__main__':
             start(__name__, routes, host='127.0.0.1')
     """
+
     app = Flask(__name__)
     for route in routes:
-        app.add_url_rule(route.path, host, route.f)
-    app.run(host=host)
+        app.add_url_rule(route.path, endpoint=route.path, view_func=route.f)
 
+    app.run(host=host)
